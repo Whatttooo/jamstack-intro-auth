84503af2451e73fd5588c9e7e61ee47fd886a486
Josh Whateley <j.whateley@hotmail.co.nz>
2020-05-12 15:45:49 +1200

    authenticated Dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,27 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Router } from "@reach/router";
+import IdentityModal from "react-netlify-identity-widget";
 import Layout from "../components/layout";
 import Profile from "../components/profile";
+import PrivateRoute from "../components/privateRoute";
 import RouteBase from "../components/routeBase";
 import RouteSecret from "../components/routeSecret";
 import RouteLogin from "../components/routeLogin";
 import { navigate } from "gatsby";
 
+import "react-netlify-identity-widget/styles.css";
+
 const Dashboard = ({ location }) => {
+  const [isVisible, setVisible] = useState(false);
   useEffect(() => {
     if (location.pathname.match(/^\/dashboard\/?$/)) {
       navigate("/dashboard/login", { replace: true });
     }
   }, []);
+
+  const toggleModal = () => setVisible(!isVisible);
+
   return (
     <Layout>
-      <Profile />
+      <Profile showModal={toggleModal} />
       <Router>
-        <RouteBase path="/dashboard/base" />
-        <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
+        <PrivateRoute path="/dashboard/base" component={RouteBase} />
+        <PrivateRoute path="/dashboard/secret" component={RouteSecret} />
+        <RouteLogin path="/dashboard/login" showModal={toggleModal} />
       </Router>
-      <p>Yooooooooooooooo!</p>
+      <IdentityModal showDialog={isVisible} onCloseDialog={toggleModal} />
     </Layout>
   );
 };
